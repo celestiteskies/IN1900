5611d876e37a18fb4c94ecbaf347de03b648d9da
celestiteskies <61025002+celestiteskies@users.noreply.github.com>
2020-12-17 20:22:29 +0100

    Create interest_rate.py

---
+++ week 35/interest_rate.py
@@ -0,0 +1,13 @@
+#1.6 from Langtangen or 2.2, page 3, from oppgaveheftet
+
+A= 1000       #initial amount in euros
+p=5         #interest rate in %
+n=3           #years
+
+amount = A*(1+p/100)**n
+print(f"The growth of money in a bank is: {amount}.")
+
+"""
+printed:
+The growth of money in a bank is: 1157.6250000000002.
+"""
