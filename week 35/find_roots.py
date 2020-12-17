6de385178b84273dde37d4933d4221a89f558a84
celestiteskies <61025002+celestiteskies@users.noreply.github.com>
2020-12-17 20:19:50 +0100

    Create find_roots.py

---
+++ week 35/find_roots.py
@@ -0,0 +1,14 @@
+#Problem 2.4 (find_roots.py, side 4) fra oppgaveheftet
+
+import cmath           #import complex math module
+
+a = 6
+b = -5
+c = 1
+
+d = (b**2) - (4*a*c)  # calculate the discriminant
+
+x1 = (-b-cmath.sqrt(d))/(2*a)
+x2 = (-b+cmath.sqrt(d))/(2*a)
+
+print (f"The solutions are {x1.real:.2f}, {x2.real:.2f}.")
